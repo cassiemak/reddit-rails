e5440c3a3d9478d953f87bc7309c24e78a8d425f
cassiemak <cassie.mak@gmail.com>
2014-12-15 13:47:37 +0800

    vote works

--- app/controllers/post_votes_controller.rb
+++ app/controllers/post_votes_controller.rb
@@ -7,6 +7,8 @@ class PostVotesController < ApplicationController
     new_vote = PostVote.new( :user => current_user, :post => post)
     # new_vote = PostVote.new (:user_id => current_user.id, :post_id => post.id)
 
+    # key => value
+
     if new_vote.save
       # this single line already did the view for us
       render json: new_vote, status: 201
--- app/models/post_vote.rb
+++ app/models/post_vote.rb
@@ -1,4 +1,4 @@
-class PostVotes < ActiveRecord::Base
+class PostVote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 end
